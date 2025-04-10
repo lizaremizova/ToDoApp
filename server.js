ab5bb78875ce4551ef14f9762ad99702753c3dee
23DP2ERemi <A231498ER@rvt.lv>
2025-04-10 18:03:23 +0300

    api finished

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
-const db = mongoose.connection;
-db.on('error', (error) => console.error('MongoDB connection error:', error));
-db.once('open', () => console.log('Server connected to MongoDB'));
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+// const db = mongoose.connection;
+// db.on('error', (error) => console.error('MongoDB connection error:', error));
+// db.once('open', () => console.log('Server connected to MongoDB'));
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, 
+  })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+  
+
 
 app.use(express.json());
 
